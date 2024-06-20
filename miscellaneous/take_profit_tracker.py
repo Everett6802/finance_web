70a1ebef177c16509de837aa5c2fad9e47d9895b
Price Tseng <everett6802@hotmail.com>
2024-06-20 16:50:25 +0800

    Read the data

--- miscellaneous/take_profit_tracker.py
+++ miscellaneous/take_profit_tracker.py
@@ -10,24 +10,61 @@ import argparse
 
 class TakeProfitTracker(object):
 
-	DEFAULT_SOURCE_FOLDERPATH =  "C:\\停利追蹤"
+	DEFAULT_DATA_FOLDERPATH =  "C:\\停利追蹤"
 	DEFAULT_SOURCE_FILENAME = "take_profile_tracker"
 	DEFAULT_SOURCE_FULL_FILENAME = "%s.xlsx" % DEFAULT_SOURCE_FILENAME
+	DEFAULT_RECORD_FILENAME = "take_profile_tracker_record"
+	DEFAULT_RECORD_FULL_FILENAME = "%s.txt" % DEFAULT_RECORD_FILENAME
 
 	def __init__(self, cfg):
 		self.xcfg = {
-			"source_folderpath": None,
+			"data_folderpath": None,
 			"source_filename": self.DEFAULT_SOURCE_FULL_FILENAME,
+			"record_filename": self.DEFAULT_RECORD_FULL_FILENAME,
 		}
 		self.xcfg.update(cfg)
-		self.xcfg["source_folderpath"] = self.DEFAULT_SOURCE_FOLDERPATH if self.xcfg["source_folderpath"] is None else self.xcfg["source_folderpath"]
+		self.xcfg["data_folderpath"] = self.DEFAULT_DATA_FOLDERPATH if self.xcfg["data_folderpath"] is None else self.xcfg["data_folderpath"]
 		self.xcfg["source_filename"] = self.DEFAULT_SOURCE_FULL_FILENAME if self.xcfg["source_filename"] is None else self.xcfg["source_filename"]
-		self.xcfg["source_filepath"] = os.path.join(self.xcfg["source_folderpath"], self.xcfg["source_filename"])
+		self.xcfg["source_filepath"] = os.path.join(self.xcfg["data_folderpath"], self.xcfg["source_filename"])
+		self.xcfg["record_filename"] = self.DEFAULT_RECORD_FULL_FILENAME if self.xcfg["record_filename"] is None else self.xcfg["record_filename"]
+		self.xcfg["record_filepath"] = os.path.join(self.xcfg["data_folderpath"], self.xcfg["record_filename"])
+
+		self.workbook = None
+		self.worksheet = None
+
+
+	@classmethod
+	def __check_file_exist(cls, filepath):
+		check_exist = True
+		try:
+			os.stat(filepath)
+		except OSError as exception:
+			if exception.errno != errno.ENOENT:
+				print("%s: %s" % (errno.errorcode[exception.errno], os.strerror(exception.errno)))
+				raise
+			check_exist = False
+		return check_exist
+
+
+	@classmethod
+	def __get_line_list_from_file(self, filepath):
+		# import pdb; pdb.set_trace()
+		if not self.__check_file_exist(filepath):
+			raise RuntimeError("The file[%s] does NOT exist" % filepath)
+		line_list = []
+		with open(filepath, 'r') as fp:
+			for line in fp:
+				if line.startswith("#"): continue
+				line = line.strip("\n")
+				if len(line) == 0: continue
+				line_list.append(line)
+		return line_list
 
 
 	def __enter__(self):
 # Open the workbook
 		self.workbook = xlrd.open_workbook(self.xcfg["source_filepath"])
+		self.worksheet = self.workbook.sheet_by_index(0)
 		return self
 
 
@@ -39,10 +76,39 @@ class TakeProfitTracker(object):
 		return False
 
 
+	def __read_worksheet(self, worksheet):
+		worksheet_data = {}
+		# import pdb; pdb.set_trace()
+		title_list = []
+# title
+		for column_index in range(1, worksheet.ncols):
+			title_value = worksheet.cell_value(0, column_index)
+			title_list.append(title_value)
+		# print(title_list)
+		# import pdb; pdb.set_trace()
+# data
+		for row_index in range(1, worksheet.nrows):
+			data_key = worksheet.cell_value(row_index, 0)
+			data_list = []
+			for column_index in range(1, worksheet.ncols):
+				data_value = worksheet.cell_value(row_index, column_index)
+				data_list.append(data_value)
+			# print("%s: %s" % (data_key, data_list))
+			data_dict = dict(zip(title_list, data_list))
+			worksheet_data[data_key] = data_dict
+		return worksheet_data
+
+
+	def track(self):
+		line_list = self.__get_line_list_from_file(self.xcfg["record_filepath"])
+# ['商品', '成交', '漲幅%', '漲跌']
+		stock_data_dict = self.__read_worksheet(self.worksheet)
+
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Print help')
 	
 	cfg = {}
 	
 	with TakeProfitTracker(cfg) as obj:
-		pass
+		obj.track()
